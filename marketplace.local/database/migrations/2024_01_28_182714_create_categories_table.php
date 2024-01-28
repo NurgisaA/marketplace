7d873cc1cc86295abb2b98a7ec43a6c9139ff321
nurgisa <apbaz.nurgisa@gmail.com>
2024-01-28 23:45:51 +0500

    Revert "change gitignore"
    This reverts commit ba169dcd0da515c560552b633ebe7197b8fc0561.

--- marketplace.local/database/migrations/2024_01_28_182714_create_categories_table.php
+++ marketplace.local/database/migrations/2024_01_28_182714_create_categories_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->string("img");
             $table->timestamps();
         });
     }
