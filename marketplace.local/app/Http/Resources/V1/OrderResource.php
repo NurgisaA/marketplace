e31966ff8a058ab05ab4f4f9fceee1669cd5ab67
nurgisa <apbaz.nurgisa@gmail.com>
2024-02-13 14:48:41 +0500

    add order resource

---
+++ marketplace.local/app/Http/Resources/V1/OrderResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "amount" => $this->amount,
+            "state" => $this->state,
+            "user" => new $this->user,
+            "products" => $this->product,
+        ];
+    }
+}
+
+
