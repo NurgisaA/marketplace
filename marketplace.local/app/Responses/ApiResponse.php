b8d560d83e54ba3a003158ad424f99d073fa9d63
nurgisa <apbaz.nurgisa@gmail.com>
2024-02-13 21:00:34 +0500

    change ApiResponse.php

---
+++ marketplace.local/app/Responses/ApiResponse.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Responses;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponse
+{
+    /**
+     *
+     * Success response
+     *
+     * @param string $message
+     * @param array $data
+     * @param $status
+     * @return JsonResponse
+     */
+    public function successResponse(string $message = "success", array $data = [], $status = 200): JsonResponse
+    {
+        return response()->json([
+            "status" => true,
+            "message" => $message,
+            "data" => $data
+        ], $status);
+    }
+
+
+    /**
+     *
+     * Error response
+     *
+     * @param string $message
+     * @param array $data
+     * @param $status
+     * @return JsonResponse
+     */
+    public function errorResponse(string $message = "error", array $data = [], $status = 406): JsonResponse
+    {
+        return response()->json([
+            "status" => false,
+            "message" => $message,
+            "data" => $data
+        ], $status);
+    }
+}
