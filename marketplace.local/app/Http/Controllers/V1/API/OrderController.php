db176986db4d51cf23d6fb080fb0a3e6a82bb1fb
nurgisa <apbaz.nurgisa@gmail.com>
2024-02-16 22:55:55 +0500

    rebase on master

--- marketplace.local/app/Http/Controllers/V1/API/OrderController.php
+++ marketplace.local/app/Http/Controllers/V1/API/OrderController.php
@@ -4,13 +4,8 @@ namespace App\Http\Controllers\V1\API;
 
 use App\Constants\OrderState;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreOrderRequest;
-use App\Http\Requests\UpdateOrderRequest;
 use App\Http\Resources\V1\OrderCollection;
 use App\Http\Resources\V1\OrderResource;
-use App\Http\Controllers\Controller;
-use App\Http\Resources\OrderCollection;
-use App\Http\Resources\OrderResource;
 use App\Models\Order;
 
 class OrderController extends Controller
