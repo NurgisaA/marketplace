fa81dff54a3b6327946813c93164b3a461dc8b96
nurgisa <apbaz.nurgisa@gmail.com>
2024-02-27 10:08:06 +0500

    +1

---
+++ marketplace.local/app/Filament/Resources/ProductResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('title')->required()->string(),
+
+                TextInput::make('price')
+                    ->required()
+                    ->numeric(),
+
+                RichEditor::make('description')
+                    ->string(),
+
+                FileUpload::make('image')
+                    ->image(),
+
+                Select::make('category')
+                    ->relationship(titleAttribute: 'title'),
+
+                Select::make('size')
+                    ->relationship(titleAttribute: 'title')
+                    ->multiple(),
+
+                Select::make('color')
+                    ->relationship(titleAttribute: 'title')
+                    ->multiple()
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('title'),
+                TextColumn::make('category.title'),
+                TextColumn::make('price'),
+            ])  
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
